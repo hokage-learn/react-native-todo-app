0303bcfd700d3d3a00f6e0e142aad0795ec29a42
hokage-learn <busayohokageogunde@gmail.com>
2025-10-31 19:29:09 +0000

    fix: add react-native-worklets-core and fix babel plugin order

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
+      "react-native-reanimated/plugin", // Must be last
     ],
   };
 };
